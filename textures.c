7c4ca22a1f59be4e5a42fe7cedad2c2ca7067e28
Markus Appel <markospoparkos@gmail.com>
2025-01-16 11:16:38 +0100

    use higher res textures

--- textures.c
+++ textures.c
@@ -1,22 +1,22 @@
 #include "textures.h"
 
 textures_s textures = {
-  .src_rects.numbers[0] = { 1, 69, 15, 15 },
-  .src_rects.numbers[1] = { 18, 69, 15, 15 },
-  .src_rects.numbers[2] = { 35, 69, 15, 15 },
-  .src_rects.numbers[3] = { 52, 69, 15, 15 },
-  .src_rects.numbers[4] = { 69, 69, 15, 15 },
-  .src_rects.numbers[5] = { 86, 69, 15, 15 },
-  .src_rects.numbers[6] = { 103, 69, 15, 15 },
-  .src_rects.numbers[7] = { 120, 69, 15, 15 },
-  .src_rects.mine = { 86, 52, 15, 15 },
-  .src_rects.mine_exploded = { 103, 52, 15, 15 },
-  .src_rects.mine_false_guess = { 120, 52, 15, 15 },
-  .src_rects.flag = { 36, 53, 12, 12 },
-  .src_rects.smiley = { 4, 28, 19, 19 },
-  .src_rects.smiley_excited = { 58, 28, 19, 19 },
-  .src_rects.smiley_cool = { 85, 28, 19, 19 },
-  .src_rects.smiley_dead = { 112, 28, 19, 19 }
+  .src_rects.numbers[0] = { 25, 36, 23, 23 },
+  .src_rects.numbers[1] = { 49, 36, 23, 23 },
+  .src_rects.numbers[2] = { 73, 36, 23, 23 },
+  .src_rects.numbers[3] = { 97, 36, 23, 23 },
+  .src_rects.numbers[4] = { 121, 36, 23, 23 },
+  .src_rects.numbers[5] = { 145, 36, 23, 23 },
+  .src_rects.numbers[6] = { 169, 36, 23, 23 },
+  .src_rects.numbers[7] = { 193, 36, 23, 23 },
+  .src_rects.mine = { 97, 60, 23, 23 },
+  .src_rects.mine_exploded = { 49, 60, 23, 23 },
+  .src_rects.mine_false_guess = { 73, 60, 23, 23 },
+  .src_rects.flag = { 27, 62, 18, 18 },
+  .src_rects.smiley = { 6, 89, 28, 28 },
+  .src_rects.smiley_excited = { 84, 89, 28, 28 },
+  .src_rects.smiley_cool = { 162, 89, 28, 28 },
+  .src_rects.smiley_dead = { 123, 89, 28, 28 }
 };
 
 void textures_load(void)
